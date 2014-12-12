d57ea9adb26099276319ad9bdf549507d496d317
Tejas Bhatt <tejas.bhatt121@gmail.com>
2014-12-11 19:03:31 -0500

    minor changes

--- public/modules/core/controllers/core.client.login.controller.js
+++ public/modules/core/controllers/core.client.login.controller.js
@@ -2,6 +2,8 @@
  * Created by TJ on 12/11/14.
  */
 
-swadhyay.controller('core.client.login.controller',function($scope){
-
+swadhyay.controller('core.client.login.controller',function($scope,$location){
+    $scope.login = function(){
+        $location.path('/products')
+    }
 });
